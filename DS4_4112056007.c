04bc06e558c80bced4e280a08917d531f5ce8451
ChipiChipiChapaChapaDubiDubiDabaDaba <90200858+Normad2005@users.noreply.github.com>
2025-06-05 20:27:44 +0800

    正確版本

--- DS4_4112056007.c
+++ DS4_4112056007.c
@@ -1,141 +1,107 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include<stdio.h>
+#include<stdlib.h>
+typedef enum{false, true} bool;
 
 typedef struct Node{
-    int order, num;
-    struct Node *next, *prev;
+	int order, num;
+	struct Node *next, *prev;
 }Node;
 
 typedef struct LinkedList{
-    struct Node *head;
+	struct Node *head;
 }LinkedList;
 
-Node *createNode(int order, int num);
-void insertNode(Node *node, Node *newNode);
-void deleteNode(LinkedList *l, Node *deleted);
-void printNode(LinkedList *l);
-int gcd(int a, int b);
-
-int ans[1000001];
-int testcase = 0;
-int main(){
-
-    FILE *fin = fopen("testcase4.txt", "r");
-    FILE *fout = fopen("output4.txt", "w");
-
-    fscanf(fin, "%d", &testcase);
-    while(testcase--){
-        int m, input;
-        int aid = 0;
-        fscanf(fin, "%d", &m);
-        fscanf(fin, "%d", &input);
-        LinkedList *l = malloc(sizeof(LinkedList));
-        l->head = createNode(1, input);
-        l->head->next = l->head;
-        l->head->prev = l->head;
-        Node *pre = l->head;
-        
-        for(int i=1;i<m;i++){
-            fscanf(fin, "%d", &input);
-            Node *nd = createNode(i+1, input);
-            insertNode(pre, nd);
-            pre = nd;
-        }
-    
-        int cnt = 0;
-        while(m>2){
-            int flag = 0;
-            Node *cur = l->head->next;
-            while(cur != l->head){
-                if(gcd(cur->prev->num, cur->num) == gcd(cur->num, cur->next->num)){
-                    if(gcd(cur->prev->num, cur->num) > 1){
-                        flag = 1;
-                        ans[aid++] = cur->next->order;
-                        deleteNode(l, cur->next);
-                        m--;
-                        cnt++;
-                    }
-                }
-                else{
-                    cur = cur->next;
-                }
-            }
-            if(gcd(cur->prev->num, cur->num) == gcd(cur->num, cur->next->num)){
-                if(gcd(cur->prev->num, cur->num) > 1){
-                    flag = 1;
-                    ans[aid++] = cur->next->order;
-                    deleteNode(l, cur->next);
-                    m--;
-                    cnt++;
-                }
-            }
-            if(!flag || m <= 2){
-                break;
-            }
-        }
-    
-        if(m == 2){
-            ans[aid++] = l->head->next->order;
-            deleteNode(l, l->head->next);
-            cnt++;
-        }
-    
-        fprintf(fout, "%d ", cnt);
-        for(int i=0;i<aid;i++){
-            fprintf(fout, "%d ", ans[i]);
-        }fprintf(fout, "\n");
-    
-    }
-
-    fclose(fin);
-    fclose(fout);
-
-    return 0;
-}
-
 Node *createNode(int order, int num){
-    Node *node = malloc(sizeof(Node));
-    if(node == NULL){
-        printf("Memory allocation failed\n");
-        return NULL;
-    }
-    node->order = order;
-    node->num = num;
-    node->prev = NULL;
-    node->next = NULL;
-    return node;
-}
-
-void insertNode(Node *node, Node *newNode){
-    newNode->prev = node;
-    newNode->next = node->next;
-    node->next->prev = newNode;
-    node->next = newNode;
-    return;
+	Node *n = (Node *)malloc(sizeof(Node));
+	n->next = NULL;
+	n->prev = NULL;
+	n->order = order;
+	n->num = num;
+	return n;
 }
 
 void deleteNode(LinkedList *l, Node *deleted){
-    deleted->prev->next = deleted->next;
-    deleted->next->prev = deleted->prev;
-    if(deleted == l->head){
-        l->head = l->head->next;
-    }
-    free(deleted);
-}
-
-void printNode(LinkedList *l){
-    Node *cur = l->head;
-    printf("%d:%d ", cur->order, cur->num);
-    for(cur=l->head->next; cur!=l->head ;cur = cur->next){
-        printf("%d:%d ", cur->order, cur->num);
-    }printf("\n");
+	deleted->prev->next = deleted->next;
+	deleted->next->prev = deleted->prev;
+	if(deleted == l->head)
+		l->head = l->head->next;
 }
 
 int gcd(int a, int b){
-    int r = a%b;
-    if(r == 0)
-        return b;
-    else
-        return gcd(b, r);
+	int r = a%b;
+	if(!r)
+		return b;
+	else
+		return gcd(b, r);
+}
+
+void insertNode(Node *node, Node *n){
+	n->next = node->next;
+	n->prev = node;
+	node->next->prev = n;
+	node->next = n;
+}
+
+FILE *fin, *fout;
+
+void sol() {
+    int ans[1000], ans_idx = 0;
+    int n, a;
+    fscanf(fin, "%d", &n);
+    fscanf(fin, "%d", &a);
+
+    LinkedList *l = (LinkedList *)malloc(sizeof(LinkedList));
+    l->head = createNode(1, a);
+    l->head->next = l->head;
+    l->head->prev = l->head;
+
+    Node *cur = l->head;
+    for (int i = 2; i <= n; i++) {
+        fscanf(fin, "%d", &a);
+        Node *new_node = createNode(i, a);
+        insertNode(cur, new_node);
+        cur = cur->next;
+    }
+    
+    cur = l->head;
+    while (n > 1) {
+        bool removed = false;
+        Node *p = cur;
+        int last_gcd = gcd(p->prev->num, p->num);
+        int cnt = n; // 保證掃一圈最多 n 次
+
+        while (cnt--) {
+            Node *next = p->next;
+            int new_gcd = gcd(p->num, next->num);
+            if (new_gcd == last_gcd && new_gcd > 1) {
+                ans[ans_idx++] = next->order;
+                deleteNode(l, next);
+                n--;
+                removed = true;
+                last_gcd = gcd(p->num, p->next->num);  // 更新下一對 GCD
+                // p 不動，繼續比較
+            } else {
+                p = p->next;
+                last_gcd = gcd(p->prev->num, p->num);
+            }
+        }
+        if (!removed)
+            break;
+    }
+
+    fprintf(fout, "%d", ans_idx);
+    for (int i = 0; i < ans_idx; i++)
+        fprintf(fout, " %d", ans[i]);
+    fprintf(fout, "\n\n");
+}
+
+int main(){
+	fin = fopen("testcase3.txt", "r");
+	//fout = fopen("output3.txt", "w");
+	int tc;
+	fscanf(fin, "%d", &tc);
+	while(tc--){
+		sol();
+	}
+	return 0;
 }
