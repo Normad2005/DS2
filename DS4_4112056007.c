31530853bdada7d9d06c977db16c263824bf126e
ChipiChipiChapaChapaDubiDubiDabaDaba <90200858+Normad2005@users.noreply.github.com>
2025-06-05 21:48:18 +0800

    真 正確版本

--- DS4_4112056007.c
+++ DS4_4112056007.c
@@ -1,107 +1,98 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef enum{false, true} bool;
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node{
-	int order, num;
-	struct Node *next, *prev;
+typedef struct Node* NodePtr;
+typedef struct Node {
+    int order, num;
+    NodePtr next, prev;
 }Node;
 
-typedef struct LinkedList{
-	struct Node *head;
-}LinkedList;
+struct LinkedList {
+    NodePtr head;
+}Cards;
 
-Node *createNode(int order, int num){
-	Node *n = (Node *)malloc(sizeof(Node));
-	n->next = NULL;
-	n->prev = NULL;
-	n->order = order;
-	n->num = num;
-	return n;
+int* deleted;
+int idx;
+
+NodePtr newNode(int order, int num){
+    NodePtr temp = malloc(sizeof(Node));
+    temp->order = order;
+    temp->num = num;
+    temp->next = temp->prev = NULL;
+    return temp;
 }
 
-void deleteNode(LinkedList *l, Node *deleted){
-	deleted->prev->next = deleted->next;
-	deleted->next->prev = deleted->prev;
-	if(deleted == l->head)
-		l->head = l->head->next;
+void delNode(NodePtr node){
+    deleted[idx++] = node->order;
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    free(node);
 }
 
-int gcd(int a, int b){
-	int r = a%b;
-	if(!r)
-		return b;
-	else
-		return gcd(b, r);
-}
-
-void insertNode(Node *node, Node *n){
-	n->next = node->next;
-	n->prev = node;
-	node->next->prev = n;
-	node->next = n;
-}
-
-FILE *fin, *fout;
-
-void sol() {
-    int ans[1000], ans_idx = 0;
-    int n, a;
-    fscanf(fin, "%d", &n);
-    fscanf(fin, "%d", &a);
-
-    LinkedList *l = (LinkedList *)malloc(sizeof(LinkedList));
-    l->head = createNode(1, a);
-    l->head->next = l->head;
-    l->head->prev = l->head;
-
-    Node *cur = l->head;
-    for (int i = 2; i <= n; i++) {
-        fscanf(fin, "%d", &a);
-        Node *new_node = createNode(i, a);
-        insertNode(cur, new_node);
-        cur = cur->next;
-    }
-    
-    cur = l->head;
-    while (n > 1) {
-        bool removed = false;
-        Node *p = cur;
-        int last_gcd = gcd(p->prev->num, p->num);
-        int cnt = n; // 保證掃一圈最多 n 次
-
-        while (cnt--) {
-            Node *next = p->next;
-            int new_gcd = gcd(p->num, next->num);
-            if (new_gcd == last_gcd && new_gcd > 1) {
-                ans[ans_idx++] = next->order;
-                deleteNode(l, next);
-                n--;
-                removed = true;
-                last_gcd = gcd(p->num, p->next->num);  // 更新下一對 GCD
-                // p 不動，繼續比較
-            } else {
-                p = p->next;
-                last_gcd = gcd(p->prev->num, p->num);
-            }
-        }
-        if (!removed)
-            break;
-    }
-
-    fprintf(fout, "%d", ans_idx);
-    for (int i = 0; i < ans_idx; i++)
-        fprintf(fout, " %d", ans[i]);
-    fprintf(fout, "\n\n");
+int GCD(int a, int b) {
+	if (b) while((a %= b) && (b %= a));
+	return a + b;
 }
 
 int main(){
-	fin = fopen("testcase3.txt", "r");
-	//fout = fopen("output3.txt", "w");
-	int tc;
-	fscanf(fin, "%d", &tc);
-	while(tc--){
-		sol();
-	}
-	return 0;
+    FILE *fin = fopen("testcase.txt", "r");
+    freopen("fuck.txt", "w", stdout);
+
+    int t, m, a, removed, preG, curG;
+    NodePtr lastNode, temp, cur, prev;
+
+    fscanf(fin, "%d", &t);
+    while (t--){    
+        fscanf(fin, "%d", &m);
+        deleted = malloc(sizeof(int) * m);
+        idx = 0;
+
+        Cards.head = newNode(0, -1);
+        lastNode = Cards.head;
+        for (int i = 1; i <= m; i++){
+            fscanf(fin, "%d", &a);
+            temp = newNode(i, a);
+            temp->prev = lastNode;
+            lastNode->next = temp;
+            lastNode = temp;
+        }
+        lastNode->next = Cards.head;
+        Cards.head->prev = lastNode;
+
+        removed = 2;
+        preG = 0;
+        cur = Cards.head->next->next;
+        while (removed > 0 && cur->next->next != cur){
+            removed -= 1;
+            while (cur != Cards.head){
+                prev = cur->prev == Cards.head ? cur->prev->prev : cur->prev;
+                curG = GCD(prev->num, cur->num);
+                if (curG == preG && curG > 1) {
+                    cur = cur->next;
+                    delNode(cur->prev);
+                    removed = 1;
+                }else {
+                    cur = cur->next;
+                    preG = curG;
+                }
+            }
+            cur = cur->next;
+        }
+
+        printf("%d ", idx);
+        for (int i = 0; i < idx; i++)
+            printf("%d ", deleted[i]);
+        putchar('\n');
+
+        for (cur = Cards.head->next; cur != Cards.head; ){
+            temp = cur;
+            cur = cur->next;
+            free(temp);
+        }
+        free(Cards.head);
+        free(deleted);
+    }
+
+    fclose(fin);
+    return 0;
 }
